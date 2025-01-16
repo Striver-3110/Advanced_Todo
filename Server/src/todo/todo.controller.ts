3df80391a1d4a28ff3bab7be824995f8940acfa9
Striver-3110 <21csjay067@ldce.ac.in>
2025-01-16 20:09:40 +0530

    crud: todo up and running fine!

--- Server/src/todo/todo.controller.ts
+++ Server/src/todo/todo.controller.ts
@@ -8,27 +8,28 @@ import { CreateTodoDto, UpdateTodoDto } from './todo.dto';
 export class TodoController {
     constructor(private readonly todoService: TodoService){};
 
-    @Get('/todos')
+    @Get('/getAllTodos')
     async findAll():Promise<Todo[]>{
         return this.todoService.findAll();
     }
 
-    @Get(':id')
-    async findOne(id: string):Promise<Todo>{
+    @Get('getTodo/:id')
+    async findOne(@Param('id')id: string):Promise<Todo>{
+        console.log(id)
         return this.todoService.findOne(id);
     }
 
-    @Post()
+    @Post('/addTodo')
     async create(@Body() createTodoDto: CreateTodoDto):Promise<Todo>{
         return this.todoService.create(createTodoDto);
     }
     
-    @Put(':id')
+    @Put('updateTodo/:id')
     async update(@Param('id') id: string ,@Body()updateTodoDto: UpdateTodoDto):Promise<Todo>{
         return this.todoService.update(id,updateTodoDto);
     }
 
-    @Delete(':id')
+    @Delete('deleteTodo/:id')
     async remove(@Param('id') id: string):Promise<void>{
         this.todoService.remove(id);
     }
